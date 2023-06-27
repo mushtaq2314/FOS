48dd1eab40794ff5571ab9242ddbfecf1e5b5f84
mushtaq2314 <mohammedd.mushtaq@gmail.com>
2023-06-27 12:08:14 +0530

    new

--- FOS/urls.py
+++ FOS/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-admin.site.site_header='Monu Masala Food Ordering System'
-admin.site.site_title='Monu Masala'
-admin.site.index_title='Welcome to Monu Masala'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
